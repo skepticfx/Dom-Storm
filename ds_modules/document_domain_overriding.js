f2143ee71a1328a1907e09569757ac8b5b4ec5b7
skepticfx <ahamednafeez@gmail.com>
2013-11-19 02:28:46 +0530

    Added ds_modules_scripts
    And some results and simple table library under results.js

--- ds_modules/document_domain_overriding.js
+++ ds_modules/document_domain_overriding.js
@@ -1,8 +1,20 @@
 // document_domain_overriding.js
 console.log("document_domain_overriding called");
 
+// Global variables to be set.
+
+var MODULE_NAME = "Document.Domain Overriding";
+var MODULE_DESCRIPTION = "Checks whether document.domain can be overriden from the same domain.";
+
 // Has to come from DB.
-var results = 123;
+
+// Library
+
+
+// User can call this in the script
+var results = {};
+results.state = 'NOT_STARTED';
+
 
 // Where the user really writes a scanner.
 // Users can write just some JS Files to scan stuff.
@@ -12,8 +24,13 @@ var test = function(){
 	
 
 
-console.log('');
+console.log('1');
 }
 
 
-exports = module.exports = { results: results.toString() };
+exports = module.exports = { 
+	results: results, 
+	module_name: MODULE_NAME.toString(), 
+	module_description: MODULE_DESCRIPTION.toString()
+
+};
--- ds_modules_scripts/M_xhr_unsafe_headers.js
+++ ds_modules_scripts/M_xhr_unsafe_headers.js
@@ -11,8 +11,7 @@ var MODULE_DESCRIPTION = "Checks the allowed list of unsafe headers by the XMLHt
 // Or they can rather create one from an UI. PS: this is better.
 // This specific scan also requires a Server-Side to respond with what all the request headers, the client sent, because we don't have DOM Exceptions for XHR :(
 // Has to come from DB.
-var results = {};
-results.state = 'NOT_STARTED';
+var results = "";
 
 // Where the user really writes a scanner.
 // Users can write just some JS Files to scan stuff.
@@ -27,7 +26,7 @@ console.log('1');
 
 
 exports = module.exports = { 
-	results: results, 
+	results: results.toString(), 
 	module_name: MODULE_NAME.toString(), 
 	module_description: MODULE_DESCRIPTION.toString()
 
